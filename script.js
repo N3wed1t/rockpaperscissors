19e7198be0b3a4b0af7e013db48c7e65c5a321c3
N3wed1t <sippawit.c@kkumail.com>
2023-04-25 15:25:50 +0700

    Add playRound function to script.js

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
 function getComputerChoice(){
     const data = ['Rock', 'Paper', 'Scissors'];
     const result = data[Math.floor(Math.random() * 3)];
-    console.log(result);
+    return result;
 }
+
+function playRound(playerSelection, computerSelection){
+    if(playerSelection.toUpperCase() === 'ROCK'){
+        if(computerSelection.toUpperCase() === 'SCISSORS'){
+            return 'You win! Rock beats Scissors';
+        }else if(computerSelection.toUpperCase() === 'PAPER'){
+            return 'You lose! Paper beats Rock';
+        }else {
+            return 'Draw!';
+        }
+    }else if(playerSelection.toUpperCase() === 'PAPER'){
+        if(computerSelection.toUpperCase() === 'ROCK'){
+            return 'You win! Paper beats Rock';
+        }else if(computerSelection.toUpperCase() === 'SCISSORS'){
+            return 'You lose! Scissors beats Paper';
+        }else {
+            return 'Draw!';
+        }
+    }else if(playerSelection.toUpperCase() === 'SCISSORS'){
+        if(computerSelection.toUpperCase() === 'PAPER'){
+            return 'You win! Scissors beats Paper';
+        }else if(computerSelection.toUpperCase() === 'ROCK'){
+            return 'You lose! Rock beats Scissors';
+        }else {
+            return 'Draw!';
+        }
+    }
+}
+
+const playerSelection = 'Paper';
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
