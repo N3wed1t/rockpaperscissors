dfa5c4163465331a9284b7291c980939bb1053b5
N3wed1t <sippawit.c@kkumail.com>
2023-04-30 17:36:33 +0700

    Add summary heading and game logic

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function playRound(playerSelection, computerSelection){
             comScore++;
             return 'You lose! Paper beats Rock';
         }else {
-            return 'Draw!';
+            return 'Draw, Try again!';
         }
     }else if(playerSelection.toUpperCase() === 'PAPER'){
         if(computerSelection.toUpperCase() === 'ROCK'){
@@ -26,7 +26,7 @@ function playRound(playerSelection, computerSelection){
             comScore++;
             return 'You lose! Scissors beats Paper';
         }else {
-            return 'Draw!';
+            return 'Draw, Try again!';
         }
     }else if(playerSelection.toUpperCase() === 'SCISSORS'){
         if(computerSelection.toUpperCase() === 'PAPER'){
@@ -36,19 +36,53 @@ function playRound(playerSelection, computerSelection){
             comScore++;
             return 'You lose! Rock beats Scissors';
         }else {
-            return 'Draw!';
+            return 'Draw, Try again!';
         }
     }
 }
 
+const body = document.querySelector('body');
 const buttons = document.querySelectorAll('button');
 const result = document.querySelector('.result');
+const score = document.querySelector('.score');
+const head = document.createElement('h2');
+let finish = false;
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         result.textContent = playRound(button.id.toString(), getComputerChoice());
+        result.style.color = 'black';
+
+        if(finish == true){
+            playerScore = 0;
+            comScore = 0;
+            head.style.color = 'white';
+            finish = false;
+        }else{
+            if(playerScore >= 5){
+                head.textContent = 'Congratulation!! You win!!';
+                head.style.fontSize = '40px';
+                head.classList.add('summary');
+                head.style.color = 'black';
+                body.insertBefore(head, body.firstChild);
+                finish = true;
+            }else if(comScore >= 5){
+                head.textContent = 'Unfortunately!! You lose!!';
+                head.style.fontSize = '40px';
+                head.classList.add('summary');
+                head.style.color = 'black';
+                body.insertBefore(head, body.firstChild);
+                finish = true;
+            }
+        }
+        score.textContent = `Player Score: ${playerScore} | Computer Score: ${comScore}`;
+        score.style.color = 'black';
     })
 });
+
+
+
+
 
 
 
